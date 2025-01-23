d77d21d1b36a7778c579786cc8871293f1e79fef
binh <binhtdhe150589@fpt.edu.vn>
2025-01-23 10:00:28 +0700

    video29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,7 @@
+const MyComponent = () => {
+    return (
+        <div>binh binh</div>
+    );
+}
+
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,13 @@
+const SecondComponent = () => {
+    return (
+        <div>Second component</div>
+    );
+}
+
+const ThirdComponent = () => {
+    return (
+        <div>Third component</div>
+    );
+}
+
+export { SecondComponent, ThirdComponent };
