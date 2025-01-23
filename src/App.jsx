b04189a0fdfb9029dc30b587bddb4d25e47ded72
binh <binhtdhe150589@fpt.edu.vn>
2025-01-23 18:11:21 +0700

    video48

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TodoNew from './components/todo/TodoNew'
 import './components/todo/todo.css'
 import Header from './components/layout/header'
 import Footer from './components/layout/footer'
+import { Outlet } from 'react-router-dom'
 
 
 const App = () => {
@@ -65,6 +66,7 @@ const App = () => {
         </div>
       } */}
       </div>
+      <Outlet />
       <Footer />
     </>
   )
