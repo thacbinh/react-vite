b12383bacbc4a8407045e201357d44cba8df83b7
binh <binhtdhe150589@fpt.edu.vn>
2025-01-24 11:49:35 +0700

    video69

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,13 +1,22 @@
 import { Button, Input } from "antd";
 import { useState } from "react";
+import axios from "axios";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
+    const [phone, setPhone] = useState("");
     const handleOnClickBtn = () => {
-        console.log(">>>>My data:", { fullName, email, password, phoneNumber })
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phone
+        }
+        axios.post(URL_BACKEND, data);
+        console.log(">>>>My data:", { fullName, email, password, phone })
     }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
@@ -26,7 +35,7 @@ const UserForm = () => {
                 </div>
                 <div>
                     <span>Phone number</span>
-                    <Input value={phoneNumber} onChange={(event) => setPhoneNumber(event.target.value)} />
+                    <Input value={phone} onChange={(event) => setPhone(event.target.value)} />
                 </div>
                 <div>
                     <Button onClick={handleOnClickBtn} type="primary"> Create User </Button>
