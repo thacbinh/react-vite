5cba2e35c17d89c4037fa2763bdffaa9eaca264b
binh <binhtdhe150589@fpt.edu.vn>
2025-02-03 15:41:25 +0700

    video89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,65 @@
+import { Button, Input, Form } from "antd";
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log(">>> check values: ", values)
+    }
+
     return (
-        <div>Register Page</div>
-    );
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        >
+            <div style={{
+                margin: "50px",
+            }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    label="Phone number"
+                    name="phone"
+                >
+                    <Input />
+                </Form.Item>
+
+
+                <div>
+                    <Button
+                        onClick={() => form.submit()}
+                        type="primary">Register</Button>
+                </div>
+            </div>
+        </Form>
+    )
 }
 
 export default RegisterPage;
